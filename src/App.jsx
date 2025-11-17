1be57aa8ea6e79bac0342bdc40055ef5e71f08a9
Javid Izadfar <javid.izadfar@gmail.com>
2025-11-17 11:40:45 +0330

    add s24

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-// import State from "./components/State";
-// import Search from "./components/Search";
-// import WordCount from "./components/WordCount";
-
-import { useState } from "react";
-import { Faker, fa, base } from "@faker-js/faker";
-import Clock from "./components/Clock";
-
-const faker = new Faker({
-  locale: [base, fa]
-});
-const text = faker.lorem.paragraph();
+import SignupForm from "./components/SignupForm";
 
 const App = () => {
-  const [clockIsVisible, setClockIsVisible] = useState(false);
-
-  const onToggleClick = () => {
-    setClockIsVisible(!clockIsVisible);
-  };
-
   return (
-    <>
-      {text}
-      <br />
-      {clockIsVisible && <Clock />}
-      <button onClick={onToggleClick}>Toggle</button>
-      {/* <State /> */}
-      {/* <Search /> */}
-      {/* <WordCount /> */}
-    </>
+    <div className="bg-gray-300">
+      <div className="w-96 mx-auto">
+        <SignupForm />
+      </div>
+    </div>
   );
 };
 
