38b1631028c2f44a223495b3859b44604339e3b1
Javid Izadfar <javid.izadfar@gmail.com>
2025-12-01 08:35:46 +0330

    add s28

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./assets/index.css";
-import Canban from "./views/Canban";
+import Memory from "./views/Memory";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Canban />
+      {/* <Canban /> */}
+      <Memory />
     </BrowserRouter>
   </StrictMode>
 );
